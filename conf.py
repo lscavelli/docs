7cd13d95cace9cf948c3c35b0797ada03b808524
LFG Scavelli <newportalclub@gmail.com>
2017-12-05 19:03:28 +0100

    Insert page portlets

--- conf.py
+++ conf.py
@@ -1,5 +1,9 @@
+
+# load PhpLexer
 from sphinx.highlighting import lexers
 from pygments.lexers.web import PhpLexer
+
+# enable highlighting for PHP code not between <?php ... ?> by default
 lexers['php'] = PhpLexer(startinline=True, linenos=1)
 lexers['php-annotations'] = PhpLexer(startinline=True, linenos=1)
 
@@ -114,7 +118,6 @@ html_sidebars = {
         'navigation.html',
         'relations.html',  # needs 'show_related': True theme option to display
         'searchbox.html',
-        'donate.html',
     ]
 }
 
